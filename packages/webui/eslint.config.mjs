8e3bd3ea0ab467d0f4ca1bdf13efbccb7c8d4c90
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-04-01 09:51:46 -0500

    Add eslint to all packages

--- packages/webui/eslint.config.mjs
+++ packages/webui/eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslint from "@eslint/js";
+import tselint from "typescript-eslint";
 
-const eslintConfig = defineConfig([
+const eslintConfig = defineConfig(eslint.configs.recommended, tselint.configs.recommended, [
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
