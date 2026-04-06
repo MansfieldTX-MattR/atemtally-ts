87b29d918e596b22fdd6d1157f5a34a0ec61610c
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-04-06 14:07:33 -0500

    Add typed linting to eslint configs

--- packages/server/eslint.config.mjs
+++ packages/server/eslint.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 
-import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
+import js from "@eslint/js";
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 );
--- packages/webui/eslint.config.mjs
+++ packages/webui/eslint.config.mjs
@@ -1,14 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import eslint from "@eslint/js";
+import js from "@eslint/js";
 import tselint from "typescript-eslint";
 
-const eslintConfig = defineConfig(eslint.configs.recommended, tselint.configs.recommended, [
+const eslintConfig = defineConfig([
+  js.configs.recommended,
+  ...tselint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
+    "eslint.config.mjs",
+    "next.config.ts",
+    "postcss.config.mjs",
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
