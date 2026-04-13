06292b5321bc4001128b8836b5ca6b64ce5485f2
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-04-13 12:28:31 -0500

    Force dynamic rendering so the api response is only used at runtime

--- packages/webui/app/page.tsx
+++ packages/webui/app/page.tsx
@@ -5,6 +5,8 @@ import TallyDashboard from "./components/TallyDashboard";
 import { getWebsocketUri } from "./actions";
 import type { GetTSLMapResponse } from "@atemtally/common";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   let initialMap: GetTSLMapResponse = {};
   try {
