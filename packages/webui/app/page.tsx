6e7e6521f7085e4c2500d5b633d98f1203b79971
MansfieldTX-MattR <matt.reid@mansfieldtexas.gov>
2026-04-27 17:02:56 -0500

    Remove `getWebsocketUri` (forgotten from 2e998b04)

--- packages/webui/app/page.tsx
+++ packages/webui/app/page.tsx
@@ -14,8 +14,6 @@ export default async function Home() {
     // API server may not be running yet; start with empty map
   }
 
-  const websocketUri = await getWebsocketUri();
-
   return (
     <div className="flex flex-col flex-1 items-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col gap-8 py-12 px-6 bg-white dark:bg-black">
